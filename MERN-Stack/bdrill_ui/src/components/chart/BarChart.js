71433db8b27c5628d06314ad8a3f62cd3b257e29
sahertanveer <xahert@gmail.com>
2020-06-02 20:40:27 +0500

    Admin Panel visualization

--- MERN-Stack/bdrill_ui/src/components/chart/BarChart.js
+++ MERN-Stack/bdrill_ui/src/components/chart/BarChart.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react'
-import { BrowserRouter 
-} from 'react-router-dom'
-import { Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import {tacticsBarGraph} from '../../actions/visualizationAction'
-
-
+import { tacticsBarGraph } from '../../actions/visualizationAction'
 
 class BarChart extends Component {
   constructor(props) {
@@ -14,77 +10,76 @@ class BarChart extends Component {
     this.props.tacticsBarGraph();
   }
   static defaultProps = {
-    displayTitle:true,
+    displayTitle: true,
     displayLegend: true,
-    legendPosition:'bottom',
+    legendPosition: 'bottom',
   }
 
-  
-    render() { 
-      return ( 
-        <div className=" center bar" style={{width: '90%'}}>
-          {this.props.visualization.tacticsFound ?
+  render() {
+    return (
+      <div className=" center bar" style={{ width: '90%' }}>
+        {this.props.visualization.tacticsFound ?
           <Bar
             data={
               {
-        labels: ['Initial Access', 'Execution', 'Persistence', 'Privilege Escalation', 'Defense Evasion', 'Credential Access', 'Discovery','Lateral Movement', 'Collection', 'Command and Control', 'Exfiltration', 'Impact'],
-        datasets: [
-          {
-            label: 'Tactics',
-            data: this.props.visualization.tactics,//[65, 29, 66, 56, 55, 50, 98, 65, 45, 59, 72],
-            backgroundColor:[
-                'rgb(107,142,35)', 
-                'rgb(139,0,139)', 
-                'rgb(199,21,133)', 
-                'rgb(188,143,143)', 
-                'rgb(178,34,34)', 
-                'rgb(255,160,122)', 
-                'rgb(32,178,170)', 
-                'rgb(160,82,45)',
-                'rgb(199,21,122)', 
-                'rgb(139,59,139)',
-                'rgb(255,140,122)',
-                'rgb(225,110,152)',   
-            ],
-            borderColor:[
-              'rgb(107,142,35)', 
-              'rgb(139,0,139)', 
-              'rgb(199,21,133)', 
-              'rgb(188,143,143)', 
-              'rgb(178,34,34)', 
-              'rgb(255,160,122)', 
-              'rgb(32,178,170)', 
-              'rgb(160,82,45)',
-              'rgb(199,21,122)', 
-              'rgb(139,59,139)',
-              'rgb(255,140,122)', 
-              'rgb(215,180,162)', 
-              ]
-          }
-        ]
-      }
+                labels: ['Initial Access', 'Execution', 'Persistence', 'Privilege Escalation', 'Defense Evasion', 'Credential Access', 'Discovery', 'Lateral Movement', 'Collection', 'Command and Control', 'Exfiltration', 'Impact'],
+                datasets: [
+                  {
+                    label: 'Tactics',
+                    data: this.props.visualization.tactics,//[65, 29, 66, 56, 55, 50, 98, 65, 45, 59, 72],
+                    backgroundColor: [
+                      'rgb(107,142,35)',
+                      'rgb(139,0,139)',
+                      'rgb(199,21,133)',
+                      'rgb(188,143,143)',
+                      'rgb(178,34,34)',
+                      'rgb(255,160,122)',
+                      'rgb(32,178,170)',
+                      'rgb(160,82,45)',
+                      'rgb(199,21,122)',
+                      'rgb(139,59,139)',
+                      'rgb(255,140,122)',
+                      'rgb(225,110,152)',
+                    ],
+                    borderColor: [
+                      'rgb(107,142,35)',
+                      'rgb(139,0,139)',
+                      'rgb(199,21,133)',
+                      'rgb(188,143,143)',
+                      'rgb(178,34,34)',
+                      'rgb(255,160,122)',
+                      'rgb(32,178,170)',
+                      'rgb(160,82,45)',
+                      'rgb(199,21,122)',
+                      'rgb(139,59,139)',
+                      'rgb(255,140,122)',
+                      'rgb(215,180,162)',
+                    ]
+                  }
+                ]
+              }
             }
             options={{
               maintainAspectRatio: false
             }}
           />
-        : ""}
-        </div>
-      );
-    }
+          : ""}
+      </div>
+    );
   }
+}
 
-  BarChart.propTypes = {
+BarChart.propTypes = {
 
-    tacticsBarGraph: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    page: PropTypes.object.isRequired,
+  tacticsBarGraph: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  page: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    visualization: state.visualization,
-    page: state.page
+  auth: state.auth,
+  visualization: state.visualization,
+  page: state.page
 })
 
-export default (connect(mapStateToProps, {tacticsBarGraph})(BarChart))
+export default (connect(mapStateToProps, { tacticsBarGraph })(BarChart))
