0a913e4cdfbca5bb209610b73cd9096ad46a6c24
Muhammad Haider Ali Khan <mmali277@gmail.com>
2020-06-13 21:15:24 +0500

    Schedule Calendar Displayed for admin

--- MERN-Stack/bdrill_ui/src/components/admin/PendingAssignments.js
+++ MERN-Stack/bdrill_ui/src/components/admin/PendingAssignments.js
@@ -3,9 +3,13 @@ import { BrowserRouter, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { BackendInstance } from '../../config/axiosInstance';
-import { deleteAssignment } from '../../actions/managerAuthAction'
+import { deleteAssignment } from '../../actions/managerAuthAction';
+import { setAlert } from '../../actions/alertAction'
+import Alert from '../../layout/Alert'
 
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from "@syncfusion/ej2-react-schedule";
 import moment from "moment";
+
 //paths
 
 class PendingAssignments extends Component {
@@ -13,12 +17,18 @@ class PendingAssignments extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            assignments: null
+            assignments: null,
+            schedules: [],
+            from: Date.now(),
+            to: "all"
         }
 
         this.deleteassignment = this.deleteassignment.bind(this);
         this.getAssignments = this.getAssignments.bind(this);
-        this.getAssignments()
+        this.getSchedule = this.getSchedule.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.getAssignments();
+        this.getSchedule();
     }
 
     getAssignments = () => {
@@ -31,16 +41,54 @@ class PendingAssignments extends Component {
                 this.setState({ assignments: response.data.assignments })
 
             })
-            .catch(err => alert(err + 'session cannot be initiated'));
-   }
+            .catch(err => {
+                const errors = err.response.data.errors;
+        if (errors) {
+            errors.forEach(error => {  this.props.setAlert(err.response.data.msg, 'danger') })
+        }
+               
+            });
+    }
 
-   deleteassignment= async (e) => {
-       e.preventDefault();
-       await this.props.deleteAssignment(e.currentTarget.value);
-       this.getAssignments();
-   }
+    deleteassignment = async (e) => {
+        e.preventDefault();
+        await this.props.deleteAssignment(e.currentTarget.value);
+        this.getAssignments();
+    }
 
+    getSchedule = async () => {
+        const config = {
+            headers: {
+                'Content-Type': ' application/json '
+            }
+        }
 
+        const body = JSON.stringify({ from: this.state.from, to: this.state.to });
+
+        BackendInstance.post('api/admin/getschedule', body, config)
+            .then(response => {
+                this.setState({ schedules: response.data.schedules })
+
+            })
+            .catch(err => {
+                this.props.setAlert(err, 'danger')
+        //         const errors = err.response.data.errors;
+        // if (errors) {
+        //     errors.forEach(error => {  this.props.setAlert(err.response.data.msg, 'danger') })
+        // }
+               
+            });
+    }
+    
+
+    onChange = e =>
+        this.setState({ [e.target.name]: e.target.value });
+    
+        onSubmit = e => {
+            e.preventDefault();
+            this.getSchedule();
+            
+        };
 
     render() {
 
@@ -49,38 +97,90 @@ class PendingAssignments extends Component {
                 <main>
                     <div className="container-fluid" >
                         <div className="row">
-                            <div className="col s12 m12 l12">
-                            <div className="card animate fadeLeft uicards">
-                <div className="card-content">
-                                    <h3 className="card-stats-number white-text" style={{ fontFamily: "Princess Sofia" }}> Assignments</h3>
-                                    <hr />
-                                    <br/>
-                                    <table className="center">
-                                        <tr className="badge-light center">
-                                            <th>Attack_ID</th>
-                                            <th>Platform Name</th>
-                                            <th>Category</th>
-                                            <th>Subject</th>
-                                            <th>Start Time</th>
-                                            <th>End Time</th>
-                                            <th>Delete</th>
-                                        </tr>
 
-                                        {this.state.assignments ? this.state.assignments.map((field, idx) => {
-                                            return (
-                                                <tr className="  center">
-                                                    <td>{field.procedure_id}</td>
-                                                    <td>{field.platform}</td>
-                                                    <td>{field.category}</td>
-                                                    <td>{field.schedule.Subject}</td>
-                                                    <td>{moment(new Date(field.schedule.StartTime)).format('D MMM YYYY , h:mm:ss:A')}</td>
-                                                    <td>{moment(new Date(field.schedule.EndTime)).format('D MMM YYYY , h:mm:ss:A')}</td>
-                                                    <td><button className="btn btn-danger" value={field._id} onClick={(e) => { this.deleteassignment(e) }} > <i class="material-icons">delete</i></button></td>
-                                                </tr>)
-                                        }) : null}
-                                    </table>
+                            <div className="col s12 m12 l12 cardGroup center">
+                                <div className="card uicards">
+                                    <div className="card-content">
+
+
+
+                                        <h3 className="card-stats-number white-text" style={{ fontFamily: "Princess Sofia" }}>Schedule</h3>
+                                        <form className="form" onSubmit={(e)=>this.onSubmit(e)}>
+                                            <div className="row">
+                                                <div className="col s3 m4 l4">
+                                                    <input
+                                                        type="date"
+                                                        className="form-control white-text "
+                                                        name="from"
+                                                        placeholder="From"
+                                                        defaultValue={moment(new Date()).format('YYYY-MM-DD')}
+                                                        onChange={e => this.onChange(e)}
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="col s3 m4 l4">
+                                                    <h5 className="card-stats-number white-text" style={{ fontFamily: "Princess Sofia" }}>To</h5>
+                                                </div>
+                                                <div className="col s3 m4 l4">
+                                                    <input
+                                                        type="date"
+                                                        className="form-control white-text "
+                                                        name="to"
+                                                        placeholder="To"
+                                                        defaultValue={this.state.schedules && this.state.schedules.length>0 ? moment(this.state.schedules[this.state.schedules.length - 1].StartTime).format('YYYY-MM-DD'): null}
+                                                        // defaultValue={profile && profile.dob ? moment(new Date(dob)).format('YYYY-MM-DD') : ""}
+                                                        onChange={e => this.onChange(e)}
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="col s12 m12 l12">
+                                                    <button className="right btn btn-primary my-2" >Submit </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
+                                <Alert />
+                                {this.state.schedules && this.state.schedules.length>0 ?
+                                <ScheduleComponent currentView='Month' eventSettings={{dataSource: this.state.schedules}} >
+                                    {/* eventSettings={{  }} */}
+
+                                    <Inject services={[Day, Week, WorkWeek, Month, Agenda]}></Inject>
+                                </ScheduleComponent>
+    : null }
                             </div>
+                            <div className="col s12 m12 l12">
+                                <div className="card animate fadeLeft uicards">
+                                    <div className="card-content">
+                                        <h3 className="card-stats-number white-text" style={{ fontFamily: "Princess Sofia" }}> Assignments</h3>
+                                        <hr />
+                                        <br />
+                                        <table className="center">
+                                            <tr className="badge-light center">
+                                                <th>Attack_ID</th>
+                                                <th>Platform Name</th>
+                                                <th>Category</th>
+                                                <th>Subject</th>
+                                                <th>Start Time</th>
+                                                <th>End Time</th>
+                                                <th>Delete</th>
+                                            </tr>
+
+                                            {this.state.assignments ? this.state.assignments.map((field, idx) => {
+                                                return (
+                                                    <tr className="  center">
+                                                        <td>{field.procedure_id}</td>
+                                                        <td>{field.platform}</td>
+                                                        <td>{field.category}</td>
+                                                        <td>{field.schedule.Subject}</td>
+                                                        <td>{moment(new Date(field.schedule.StartTime)).format('D MMM YYYY , h:mm:ss:A')}</td>
+                                                        <td>{moment(new Date(field.schedule.EndTime)).format('D MMM YYYY , h:mm:ss:A')}</td>
+                                                        <td><button className="btn btn-danger" value={field._id} onClick={(e) => { this.deleteassignment(e) }} > <i class="material-icons">delete</i></button></td>
+                                                    </tr>)
+                                            }) : null}
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -92,6 +192,7 @@ class PendingAssignments extends Component {
 }
 PendingAssignments.propTypes = {
     deleteAssignment: PropTypes.func.isRequired,
+    setPage: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     page: PropTypes.object.isRequired
 }
@@ -102,6 +203,6 @@ const mapStateToProps = state => ({
 })
 
 //export default withRouter(CandSession);
-export default withRouter(connect(mapStateToProps, { deleteAssignment })(PendingAssignments))
+export default withRouter(connect(mapStateToProps, { deleteAssignment, setAlert })(PendingAssignments))
 
 
--- MERN-Stack/routes/api/admin.js
+++ MERN-Stack/routes/api/admin.js
@@ -11,6 +11,7 @@ const Admin = require('../../models/Admin')
 const Organization = require('../../models/Organization')
 const Manager = require('../../models/Manager')
 const User = require('../../models/User')
+const Schedule = require('../../models/Schedule')
 const auth = require('../../middleware/auth')
 router.use(cors())
 
@@ -40,62 +41,62 @@ router.post('/register',
       .withMessage('Password must include one lowercase character, one uppercase character, a number, and a special character.'),
 
   ],
-  async  (req, res) => {
-  const errors = validationResult(req);
-  
-  if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
- 
-  const { username, email, password } = req.body;
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  try{
-    //check if user exists
-    let user = await Admin.findOne({ email }) || await Organization.findOne({email}) || await Manager.findOne({ email }) || await User.findOne({email})
-    
-    if(user) {
-      return res.status(400).json({errors:[{msg:'User already exists'}]});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const avatar = null
+    const { username, email, password } = req.body;
 
-    user = new Admin({
-      username,
-      email,
-      avatar,
-      password
-    })
+    try {
+      //check if user exists
+      let user = await Admin.findOne({ email }) || await Organization.findOne({ email }) || await Manager.findOne({ email }) || await User.findOne({ email })
 
-    //Encrypy password
-
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
-
-    await user.save(); //give us promise thats wht await is used
-
-    //Return jsonwebtoken
-    const payload = {
-      user: {
-        id: user.id,
-        username: user.username
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
       }
+
+      const avatar = null
+
+      user = new Admin({
+        username,
+        email,
+        avatar,
+        password
+      })
+
+      //Encrypy password
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save(); //give us promise thats wht await is used
+
+      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+          username: user.username
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) console.log({ err });
+          res.json({ token });
+        }
+      )
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error')
     }
-
-    jwt.sign(
-      payload,
-      config.get('jwtSecret'),
-      { expiresIn: 360000 },
-      (err, token) => {
-        if(err) console.log({ err });
-        res.json({ token });
-      }
-    )
-
-  } catch(err) {
-    console.error(err.message);
-     res.status(500).send('Server Error')
-   }
-})
+  })
 
 
 // @route   GET api/admin/getassignments
@@ -109,10 +110,10 @@ router.post('/getassignments', auth, async (req, res) => {
       return res.status(422).json({ errors: errors.array() });
     }
     try {
-       await Assignments.find({end_time:{"$gte":new Date()}}).populate("schedule").then(   //populate("schedule_id",null,{EndTime:{"$gte":new Date()
-       assignments=> {
-        return res.json({ assignments });
-        }) .catch(err => { return res.status(422).json({ errors: errors.array() }) });
+      await Assignments.find({ end_time: { "$gte": new Date() } }).populate("schedule").then(   //populate("schedule_id",null,{EndTime:{"$gte":new Date()
+        assignments => {
+          return res.json({ assignments });
+        }).catch(err => { return res.status(422).json({ errors: errors.array() }) });
     }
     catch (err) {
       console.error(err.message);
@@ -128,21 +129,21 @@ router.post('/getassignments', auth, async (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.get('/', auth, async (req, res) => {
-  try{
-    if (req.user.role === "admin"){
+  try {
+    if (req.user.role === "admin") {
 
-    const user =await Admin.findById(req.user.id).select('-password');
-    let roleBasedUser = user.toObject();
-    roleBasedUser["role"] = "admin";
-    res.json(roleBasedUser);
+      const user = await Admin.findById(req.user.id).select('-password');
+      let roleBasedUser = user.toObject();
+      roleBasedUser["role"] = "admin";
+      res.json(roleBasedUser);
 
     }
-    else{
+    else {
       res.status(401).send('Unauthorized Access');
     }
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
-     res.status(500).send('Server Error')
+    res.status(500).send('Server Error')
   }
 
 });
@@ -150,65 +151,65 @@ router.get('/', auth, async (req, res) => {
 // @route   GET api/users/login
 // @desc    Login User / Returning JWT Token
 // @access  Public
-router.post('/login', 
+router.post('/login',
   [
     check('email', 'Email is required')
-    .isEmail(),
+      .isEmail(),
 
     check('password', 'Password is required')
-    .not()
-    .isEmpty(),
-    
+      .not()
+      .isEmpty(),
+
   ],
   async (req, res) => {
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  if(!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-  const { email, password } = req.body;
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
 
-  try{
-    //check if user exists
+    try {
+      //check if user exists
       let user = await Admin.findOne({ email });
 
-     
-      if(!user) {
-        return res.status(404).json({errors:[{msg:'Invalid Credentials'}]})
+
+      if (!user) {
+        return res.status(404).json({ errors: [{ msg: 'Invalid Credentials' }] })
       }
 
       //check if password matches
       const isMatch = await bcrypt.compare(password, user.password)
 
-      if(!isMatch) {
-        return res.status(404).json({errors:[{msg:'Invalid Credentials'}]})
+      if (!isMatch) {
+        return res.status(404).json({ errors: [{ msg: 'Invalid Credentials' }] })
       }
 
-       //Return jsonwebtoken
-        const payload = {
-          user: {
-            id: user.id,
-            role: "admin"
-          }
+      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+          role: "admin"
         }
+      }
 
 
-        jwt.sign(
-          payload,
-          config.get('jwtSecret'),
-          { expiresIn: 360000 },
-          (err, token) => {
-            if(err) console.log({ err });
-            res.json({ token });
-          }
-        )
-      } catch(err) {
-        console.error(err.message);
-         res.status(500).send('Server Error')
-       }
-    });
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) console.log({ err });
+          res.json({ token });
+        }
+      )
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error')
+    }
+  });
 
-    // @route   POST api/admin/getorganizations
+// @route   POST api/admin/getorganizations
 // @desc    POST All Organizations
 // @access  Public
 router.post('/getorganizations', auth, async (req, res) => {
@@ -218,7 +219,7 @@ router.post('/getorganizations', auth, async (req, res) => {
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    
+
     try {
       let org = await Organization.find({}, function (err, docs) {
         if (err) res.json(err);
@@ -243,7 +244,7 @@ router.post('/getorganizations', auth, async (req, res) => {
   }
 });
 
-    
+
 // @route   POST api/admin/getmanagers
 // @desc    POST All Managers
 // @access  Public
@@ -279,7 +280,7 @@ router.post('/getmanagers', auth, async (req, res) => {
 // @desc    Get All Candidates
 // @access  Public
 router.post('/getusers', auth, async (req, res) => {
-  if ( req.user.role === "admin" ) {
+  if (req.user.role === "admin") {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -295,7 +296,7 @@ router.post('/getusers', auth, async (req, res) => {
         .select('-manager_id')
         .select('-organization_id')
       return res.json({ users });
-      
+
     }
     catch (err) {
       console.error(err.message);
@@ -306,5 +307,66 @@ router.post('/getusers', auth, async (req, res) => {
     res.status(401).send('Unauthorized Access')
   }
 });
+
+// @route   POST api/admin/getschedule
+// @desc    POST Schedule
+// @access  Public
+router.post('/getschedule', auth, [
+  check('from', 'Start date is required')
+    .not()
+    .isEmpty(),
+  check('to', 'End date is required')
+    .not()
+    .isEmpty()
+],
+  async (req, res) => {
+    if (req.user.role === "admin") {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+      try {
+        var { from, to } = req.body;
+        let condition = {}
+        if (to === "all")
+          condition = {
+            EndTime:
+            {
+              $gte: from
+            }
+          }
+        else
+          condition = {
+            $and: [
+
+              {
+                EndTime: { $gte: from }
+              }, {
+                StartTime: { $lte: to }
+              }
+            ]
+          }
+        let schedules = await Schedule.find(condition, function (err, docs) {
+          if (err) res.json(err);
+        })
+          .select('-platform')
+          .select('-date')
+          .select('-__v')
+          .select('-candidate_id')
+          .select('-_id')
+
+        return res.json({ schedules });
+      }
+      catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error')
+      }
+    }
+    else {
+      return res.status(401).send('Unauthorized Access')
+    }
+  });
+
 
 module.exports = router
